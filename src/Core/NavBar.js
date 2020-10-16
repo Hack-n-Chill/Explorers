9dcb7a4d83b5cf74b15ddd56ac1c5eaa959bf4fa
Aniket Ghosh <ghoshaniketkumar7@gmail.com>
2020-10-16 16:08:17 +0530

    basic styling changes

--- src/Core/NavBar.js
+++ src/Core/NavBar.js
@@ -21,7 +21,7 @@ const NavBar = () => {
                         user ? (
                             <>
                                 <input className="form-control mr-sm-2" type="search" placeholder="Search Your Stock" aria-label="Search" />
-                                <button className="btn btn-light my-2 my-sm-0" type="button" onClick={searchButtonPressed} >Search</button>
+                                <button className="btn btn-light my-2 my-sm-0 font-weight-bold" type="button" onClick={searchButtonPressed} >Search</button>
                             </>
                         ) : (
                                 <>
